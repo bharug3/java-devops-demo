c888d1dd683042f2370a1a16a036b0465d1b5ec9
bharug3 <108127447+bharug3@users.noreply.github.com>
2022-07-06 22:12:43 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot, This is bhargav Gunda working on webhooks!";
+        return "Greetings from Spring Boot, This is bhargav Gunda!";
     }
 
 }
