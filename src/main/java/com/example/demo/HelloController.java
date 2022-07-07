1f8a35d7dc433ff8a0fa83ca7680d16206551064
bharug3 <108127447+bharug3@users.noreply.github.com>
2022-07-07 12:03:09 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot, This is bhargav gunda, ironman is my fav superhero!";
+        return "Greetings from Spring Boot, This is bhargav gunda, ironman is my fav superhero and pawankalyan is my fav hero!";
     }
 
 }
